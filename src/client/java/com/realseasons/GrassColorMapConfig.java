6a6e84e43516445202058e9ec2ac69644aee134c
MTaningco <jmigst@gmail.com>
2026-05-09 23:41:57 -0700

    Remove classes that are no longer used.

--- src/client/java/com/realseasons/GrassColorMapConfig.java
+++
@@ -1,8 +0,0 @@
-package com.realseasons;
-
-import java.util.Map;
-
-public class GrassColorMapConfig {
-    public Map<Integer, String> defaultGrassColorToBiomeGroupMap;
-    public Map<String, int[]> biomeIdToSeasonArrayMap;
-}
--- src/client/java/com/realseasons/GrassConfigEntry.java
+++
@@ -1,7 +0,0 @@
-package com.realseasons;
-
-public class GrassConfigEntry {
-    public String biomeId;
-    public Integer defaultDecimalColor; // nullable
-    public String[] seasonColors;
-}
